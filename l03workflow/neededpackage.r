5ad8bcd1478e54eaf47d833342eb73d6245e4c58
lidingruc <liding@ruc.edu.cn>
2018-03-13 17:56:04 +0800

    需要的包和安装方法
    添加了安装方法

--- l03workflow/neededpackage.r
+++ l03workflow/neededpackage.r
@@ -12,7 +12,7 @@ library(extrafont)  #额外字体
 
 
 #可视化
-library(gganimate)    #安装可能较难，动态
+library(gganimate)    #安装可能较难，动态图，见后面的说明
 library(animation)    #动态图
 library(lattice)      #多变量作图 
 library(RColorBrewer) #调色板
@@ -90,6 +90,9 @@ library(RSelenium)    #动态抓取、需要配套
 library(selectr)      #css标记提取
 
 
+# 包的安装
+install.packages("tidyverse")
+
 #批量安装包 示意
 install.packages(c("car", "devtools", "DiagrammeR", "effects", "ggplot2", "knitr", "lme4", "matlib", "MBESS", "mi", "polycor", "readxl", "rgl", "rmarkdown", "sem", "sfsmisc"))
 
@@ -134,4 +137,70 @@ data(tips,package='reshape2')
 #查看包的帮助
 help(package="tidyverse")
 
+#动态图包 需要依赖ImageMagick或者GraphicMagick
 
+library(gganimate)    #安装可能较难，动态图，见后面的说明
+library(animation)    #动态图
+
+# windows基于ImageMagick使用animation可能比较方便
+#Mac电脑需要有如下准备
+# 1、Mac要安装animation新版本：https://github.com/yihui/animation/releases/tag/v2.5
+# 2、Mac要安装GraphicMagick，且的路径放到PATH变量中
+# GraphicMagick默认路径在 /usr/local/Cellar/graphicsmagick/1.3.25/bin/gm  
+#Mac修改PATH的方法：http://architectryan.com/2012/10/02/add-to-the-path-on-mac-os-x-mountain-lion/#.WV8AgNOGMnU
+
+# Mac通过brew安装了GraphicsMagick是一个很复杂的问题，你需要有耐心
+# 如果出现错误可以全部删除相关命令然后重装
+#brew uninstall imagemagick graphicsmagick libpng jpeg
+#brew cleanup -s
+#brew install graphicsmagick
+
+#相关说明：http://blog.csdn.net/cloudsben/article/details/8164047
+# https://github.com/aheckmann/gm/wiki/Installing-ImageMagick---GraphicsMagick
+# https://brew.sh/
+# https://stackoverflow.com/questions/17756587/installing-graphicsmagick-on-mac-os-x-10-8
+
+
+###########
+# Rjava的安装
+
+## 1、需要下载并安装java，设定好环境变量，注意macOS sierria不能装java jdk9,如果装了，需要卸载（具体见http://www.cnblogs.com/eadwin/p/7656310.html）。
+# 安装及设置环境变量的说明：mac http://www.cnblogs.com/zhangzhangwhu/p/7171735.html windows：
+
+#  
+#   https://www.java.com/en/download/
+#   CMD中查看版本、路径信息
+#   system("java -version")
+#   $(/usr/libexec/java_home)
+
+## 2、安装rjava，留意安装的路径: 
+#   install.packages("rJava", type = "source")
+#   如果安装过可以先删除 
+#   remove.packages("rJava")
+
+## 3、设定动态库位置，让Rjava能够找到java
+#    在cmd 中输入
+#   sudo ln -f -s $(/usr/libexec/java_home)/jre/lib/server/libjvm.dylib /usr/local/lib
+#   ln -s $(/usr/libexec/java_home)/jre/lib/server/libjvm.dylib /Library/Frameworks/R.framework/Versions/3.3/Resources/lib/
+
+# 参考资料
+# https://stackoverflow.com/questions/37014340/installing-r-package-opennlp-in-r
+# https://stackoverflow.com/questions/30738974/rjava-load-error-in-rstudio-r-after-upgrading-to-osx-yosemite
+
+
+#############
+# Rselenium+ phantomjs  安装
+# 渲染+ 模拟翻页（复杂翻页）
+# 案例来自:https://mp.weixin.qq.com/s/6sZRDA8sm_a1qu46EGlusA
+# 已经验证和修改
+# 1、需要安装java才能运行这种方式
+# 2、下载selenium，放到某个目录即可
+# 下载地址 http://selenium-release.storage.googleapis.com/index.html?path=3.3/
+# 示例：https://ask.hellobi.com/blog/datamofang/10742
+# 说明：https://cran.r-project.org/web/packages/RSelenium/vignettes/RSelenium-basics.html
+# mac brew 安装测试 #http://www.cnblogs.com/richaaaard/p/5097792.html
+
+
+# mac使用brew install 包
+# http://brew.sh/   在官方网站对brew的用法进行了详细的描述
+#http://www.cnblogs.com/TankXiao/p/3247113.html
